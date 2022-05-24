f8a00d5710d98663a4b798928fcd939ba3eaccc4
Corix5 <luisdavid202075@gmail.com>
2022-05-23 22:14:54 -0500

    Metodos de recuperacion de contraseña completo y mostrar nombre de usuario

--- Proyecto/src/controllers/userInfo.controller.js
+++ Proyecto/src/controllers/userInfo.controller.js
@@ -1,8 +1,10 @@
 const userInfoCtrl = {};
 const User = require('../models/User');
+const passport = require('passport');
+const { appendFile } = require('fs');
 
 userInfoCtrl.renderUserInfo = async (req, res)=>{
-    const users = await User.findById().lean();
+    const users = await User.findOne(req.user).lean(); 
     res.render('userInfo/userInfo', {users});
 };
 
--- Proyecto/src/controllers/users.controller.js
+++ Proyecto/src/controllers/users.controller.js
@@ -3,8 +3,9 @@ const usersCtrl = {};
 const passport = require('passport');
 
 const User = require('../models/User');
+const nodemailer = require('nodemailer');
 
-usersCtrl.renderSignUpForm = (req, res) => {
+usersCtrl.renderSignUpForm = async (req, res) => {
     res.render('users/signup');
 };
 
@@ -38,6 +39,33 @@ usersCtrl.signup = async (req, res) =>{
                 const newUser = new User({name: nombre, lastnameP: apellidoPaterno, lastnameM: apellidoMaterno, ipnID: boleta, career: carrera, phone: telefono, email:correo, password: contrasena});
                 newUser.password = await newUser.encyptPassword(contrasena);
                 await newUser.save(); 
+
+                //Correo de confirmacion
+                let transporter = nodemailer.createTransport({
+                    host: "smtp.gmail.com",
+                    port: 587,
+                    secure: false, // true for 465, false for other ports
+                    auth: {
+                      user: "luisdavid201375@gmail.com", // generated ethereal user
+                      pass: "ciehfulauhibkmgv", // generated ethereal password
+                    },
+                  });
+            
+                    // send mail with defined transport object
+                let info = await transporter.sendMail({
+                from: '"ServiCom 👻" <foo@example.com>', // sender address
+                to: correo, // list of receivers
+                subject: "Bienvenido a ServiCom ✔", // Subject line
+                text: "Hello world?", // plain text body
+                html: `<b>Haz creado una cuenta en ServiCom, desde aquí podrás encontrar la vacante ideal para tu servicio social </b> `, // html body
+              });
+            
+              console.log("Message sent: %s", info.messageId);
+              // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+            
+              // Preview only available when sending through an Ethereal account
+              console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+              // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
                 res.redirect('/users/signin');
             }
     }
