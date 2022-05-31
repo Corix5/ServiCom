fa5168fbbd14f4fa5eabf5cca5049ae8fd7ab05a
Luis Coria <luisdavid202075@gmail.com>
2022-05-30 23:02:29 -0500

    Ya se pueden agregar los links de portafolio y se muestran en pantalla

--- Proyecto/src/public/JS/createInput.js
+++ Proyecto/src/public/JS/createInput.js
@@ -2,6 +2,7 @@ function createInputPortafolio(){
     let container = document.getElementById("containerPortafolio");
     let input = document.createElement("input");
     input.type = "text";
+    input.name = "repository";
     input.className = "form-control newInput";
     input.placeholder = "Ej: Repositorios de GitHub";
     container.appendChild(input);
@@ -11,6 +12,7 @@ function createInputAct(){
     let container = document.getElementById("containerAct");
     let input = document.createElement("input");
     input.type = "text";
+    input.name = "activities";
     input.className = "form-control";
     input.placeholder = "Activities a realizar";
     container.appendChild(input);
@@ -20,6 +22,7 @@ function createInputNoAct(){
     let container = document.getElementById("containerNoAct");
     let input = document.createElement("input");
     input.type = "text";
+    input.name = "noActivities";
     input.className = "form-control newInput";
     input.placeholder = "Actividades a no realizar";
     container.appendChild(input);
@@ -29,6 +32,7 @@ function createInputBenefits(){
     let container = document.getElementById("containerBenefits");
     let input = document.createElement("input");
     input.type = "text";
+    input.name = "benefits";
     input.className = "form-control newInput";
     input.placeholder = "Beneficios";
     container.appendChild(input);
